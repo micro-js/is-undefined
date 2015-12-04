1286d28958cde99aebd716fc52468829ad0f0500
Josh Taylor <joshrtay@gmail.com>
2015-12-04 00:18:29 -0800

    apply khaos template

--- test/index.js
+++ test/index.js
@@ -9,13 +9,37 @@ var test = require('tape')
  * Tests
  */
 
-test('should work', function (t) {
-  t.ok(isUndefined())
-  t.ok(isUndefined(undefined))
-  t.ok(!isUndefined(null))
-  t.ok(!isUndefined('test'))
-  t.ok(!isUndefined({}))
-  t.ok(!isUndefined([]))
-  t.ok(!isUndefined(1))
+test('empty is undefined', function (t) {
+  t.equal(isUndefined(), true)
+  t.end()
+})
+
+test('undefined is undefined', function (t) {
+  t.equal(isUndefined(undefined), true)
+  t.end()
+})
+
+test('string is undefined', function (t) {
+  t.equal(isUndefined('test'), false)
+  t.end()
+})
+
+test('null is not undefined', function (t) {
+  t.equal(isUndefined(null), false)
+  t.end()
+})
+
+test('obj is not undefined', function (t) {
+  t.equal(isUndefined({}), false)
+  t.end()
+})
+
+test('array is not undefined', function (t) {
+  t.equal(isUndefined([]), false)
+  t.end()
+})
+
+test('number is not undefined', function (t) {
+  t.equal(isUndefined(1), false)
   t.end()
 })
